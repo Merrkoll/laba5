55fb62f92ea77ddf920975365bd846d1bd2d0f0b
Merrkoll <narekv653@gmail.com>
2024-01-23 13:21:23 +0700

    First stage

---
+++ main.py
@@ -0,0 +1,30 @@
+import random
+
+
+def menu():
+    fl_for_start_alg = False
+    while True:
+        print("1. Заполнение матрицы вручную")
+        print("2. Случайное заполнение матрицы")
+        print("3. Выполнение поставлненной здачи")
+        print("4. Вывод результата")
+        print("5. Выход из программы")
+
+        choose = input("Выберите пункт меню: ")
+
+        if choose == "1":
+           pass
+        elif choose == "2":
+           pass
+        elif choose == "3":
+            pass
+        elif choose == "4":
+           pass
+        elif choose == "5":
+            pass
+        else:
+            pass
+
+
+if __name__ == "__main__":
+    menu()
